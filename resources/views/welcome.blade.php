3984c87e1e1cb2afbff17b3fa13a2cbb658f52e9
HeartlandTechie <bradley.w.miller@gmail.com>
2022-02-06 14:28:21 -0600

    Fixed centering issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
     <body class="antialiased">
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <div class="flex justify-center">
                     <img src="logo.png">
                 </div>
 
